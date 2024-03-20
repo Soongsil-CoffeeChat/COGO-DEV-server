43721b786f475aff41111d1e5cb74c9c5522bf6d
김교휘 <hwee000@naver.com>
2024-03-20 22:22:51 +0900

    init: project

---
+++ src/main/java/com/soongsil/CoffeeChat/CoffeeChatApplication.java
@@ -0,0 +1,13 @@
+package com.soongsil.CoffeeChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeChatApplication.class, args);
+	}
+
+}
