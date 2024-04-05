e990ebfc3831ad392fde3de4c29abcb2b2d16855
김교휘 <hwee000@naver.com>
2024-04-05 18:26:39 +0900

    멘토 ìdto 리스트 가져오기

--- src/main/java/com/soongsil/CoffeeChat/repository/UserRepository.java
+++ src/main/java/com/soongsil/CoffeeChat/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.soongsil.CoffeeChat.repository;
 
+import com.soongsil.CoffeeChat.entity.Mentor;
 import com.soongsil.CoffeeChat.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    User findByMentor(Mentor mentor);
 }
