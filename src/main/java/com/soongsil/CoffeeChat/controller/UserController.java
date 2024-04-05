e9231da74fb14b7f26a06a8f8006bccc6ddb748e
김교휘 <hwee000@naver.com>
2024-04-05 17:52:17 +0900

    feat : 파트별로 멘토 리ì정보 리스트 반환

--- src/main/java/com/soongsil/CoffeeChat/controller/UserController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/UserController.java
@@ -9,10 +9,7 @@ import com.soongsil.CoffeeChat.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.soongsil.CoffeeChat.enums.RequestUri.USER_URI;
 
--- src/test/java/com/soongsil/CoffeeChat/CoffeeChatApplicationTests.java
+++ src/test/java/com/soongsil/CoffeeChat/CoffeeChatApplicationTests.java
@@ -2,12 +2,17 @@ package com.soongsil.CoffeeChat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
 
 @SpringBootTest
 class CoffeeChatApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void authenticationTest(Authentication authentication){
+		System.out.println("authentication.getName() = " + authentication.getName());
 	}
 
 }
