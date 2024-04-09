6eb7fc83481fb29aa5ceb4cb45602375f2c0aaf4
김교휘 <hwee000@naver.com>
2024-04-09 20:07:46 +0900

    feat: swagger3

--- src/main/java/com/soongsil/CoffeeChat/config/SecurityConfig.java
+++ src/main/java/com/soongsil/CoffeeChat/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -100,5 +101,11 @@ public class SecurityConfig {
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**");
     }
 }
--- src/main/java/com/soongsil/CoffeeChat/controller/MentorController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/MentorController.java
@@ -12,20 +12,29 @@ import java.util.List;
 
 import static com.soongsil.CoffeeChat.enums.RequestUri.MENTOR_URI;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RequestMapping(MENTOR_URI)
 @RestController
+@Tag(name="MENTOR", description = "멘토 관련 api")
 public class MentorController {
     private final MentorService mentorService;
     public MentorController(MentorService mentorService){
         this.mentorService=mentorService;
     }
     @GetMapping("/{part}")
+    @Operation(summary="파트별 멘토 리스트 가져오기")
+    @ApiResponse(responseCode = "200", description = "DTO LIST형식으로 정보 반환")
     public ResponseEntity<List<ResponseMentorListInfo>>
     getMentorListByPart(Authentication authentication, @PathVariable("part") String part){
         return ResponseEntity.ok().body(mentorService.getMentorDtoListByPart(part));
     }
 
     @GetMapping("/possibleDates/{username}")
+    @Operation(summary="멘토의 username으로 커피챗가능시간 불러오기")
+    @ApiResponse(responseCode = "200", description = "DTO LIST형식으로 정보 반환")
     public ResponseEntity<List<PossibleDateRequestDto>> getPossibleDates
             (Authentication authentication,
              @PathVariable("username") String username){
