9bc40c9a2b7705fe6930c6f95b5d20ee76a1ea7b
JungTae Kwon <oxdjww@gmail.com>
2024-04-18 01:02:21 +0900

    refactor: 이메일 API swagger 양식 통일 (#54)

--- src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
@@ -8,16 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.soongsil.CoffeeChat.email.EmailUtil;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name="EMAIL", description = "Email 인증 관련 api")
 public class EmailController {
 
 	private final EmailUtil emailUtil;
 
 	@GetMapping(EMAIL_URI)
+	@Operation(summary = "이메일 인증하기")
+	@ApiResponse(responseCode = "200", description = "인증번호 반환")
 	public String sendAuthenticationMail(@RequestParam("email") String receiver) throws MessagingException {
 		return emailUtil.sendAuthenticationEmail(receiver);
 	}
