c05d555c3b3ebeb973ad3d41263bf4d9069b1304
김교휘 <hwee000@naver.com>
2024-05-11 18:59:31 +0900

    fix: cors err(remove crossorigin, server annotation)

--- src/main/java/com/soongsil/CoffeeChat/CoffeeChatApplication.java
+++ src/main/java/com/soongsil/CoffeeChat/CoffeeChatApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(
-	servers = {
-		@Server(url = "/", description = "Default Server url")
-	}
-)
+
 @SpringBootApplication
 public class CoffeeChatApplication {
 
