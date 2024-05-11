913611923d8f578729f63c2cfd0126d89b21704b
김교휘 <hwee000@naver.com>
2024-05-11 17:08:52 +0900

    fix: corsConfig-> add https domain

--- src/test/java/com/soongsil/CoffeeChat/CoffeeChatApplicationTests.java
+++ src/test/java/com/soongsil/CoffeeChat/CoffeeChatApplicationTests.java
@@ -1,8 +1,18 @@
 package com.soongsil.CoffeeChat;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @SpringBootTest
 class CoffeeChatApplicationTests {
@@ -13,6 +23,14 @@ class CoffeeChatApplicationTests {
 	@Test
 	void authenticationTest(Authentication authentication){
 		System.out.println("authentication.getName() = " + authentication.getName());
+	}
+
+	public abstract class CorsUtils {
+		public static boolean isPreFlightRequest(HttpServletRequest request) {
+			return (HttpMethod.OPTIONS.matches(request.getMethod()) &&
+				request.getHeader(HttpHeaders.ORIGIN) != null &&
+				request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD) != null);
+		}
 	}
 
 }
