e29a462a604a2bdca0bc5fcd080e2f5d234f03a0
김교휘 <hwee000@naver.com>
2024-05-11 15:55:38 +0900

    crossorigin빼고 corsconfig 수정

--- src/main/java/com/soongsil/CoffeeChat/controller/RefreshTokenController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/RefreshTokenController.java
@@ -18,7 +18,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @RestController  //RestController=Controller+ResponseBody
 @Tag(name="REFRESHTOKEN", description = "리프레쉬 토큰 관련 api")
-@CrossOrigin(origins = "*", maxAge = 3600)
 public class RefreshTokenController {  //Refresh토큰으로 Access토큰 발급 및 2차회원가입 컨트롤러
 	private final JWTUtil jwtUtil;
 	private final RefreshTokenService refreshTokenService;
--- src/main/java/com/soongsil/CoffeeChat/controller/StatusCheckController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/StatusCheckController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
 public class StatusCheckController {
 
 	@GetMapping("/health-check")
