a964623521fdbcb81b753a1e6309a8f741808cfe
김교휘 <hwee000@naver.com>
2024-05-15 02:52:18 +0900

    err fix: cors err by preflight options request

--- src/main/java/com/soongsil/CoffeeChat/CoffeeChatApplication.java
+++ src/main/java/com/soongsil/CoffeeChat/CoffeeChatApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-
 @OpenAPIDefinition(
 	servers = {
-		@Server(url="https://cogo.life", description = "Default Server url")
+		@Server(url = "https://cogo.life", description = "Production server"),
+		@Server(url = "http://localhost:8080", description = "Local development server")
 	}
 )
 @SpringBootApplication
