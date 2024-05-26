6b63a6d3a779a61da20ffaad45bb7f5dd98e7463
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2024-05-26 15:51:55 +0900

    멘토 시간등록 오류 해결

--- src/main/java/com/soongsil/CoffeeChat/dto/PossibleDateRequestDto.java
+++ src/main/java/com/soongsil/CoffeeChat/dto/PossibleDateRequestDto.java
@@ -6,11 +6,15 @@ import java.time.LocalTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.soongsil.CoffeeChat.entity.PossibleDate;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PossibleDateRequestDto {
 	@JsonFormat(pattern = "yyyy-MM-dd")
 	private LocalDate date;
