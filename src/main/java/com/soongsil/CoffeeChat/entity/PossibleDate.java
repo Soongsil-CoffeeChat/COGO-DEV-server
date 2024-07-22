6d4aa0347735b01652dcaedfa02169343e859474
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2024-07-23 05:19:50 +0900

    test code: 동시성 이슈 테스트 코드

--- src/main/java/com/soongsil/CoffeeChat/entity/PossibleDate.java
+++ src/main/java/com/soongsil/CoffeeChat/entity/PossibleDate.java
@@ -14,38 +14,39 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @ToString(of = {"id", "date", "startTime", "endTime", "isActive"})
 public class PossibleDate {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "possible_date_id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "possible_date_id")
+    private Long id;
 
-	@Setter
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "mentor_id")
-	private Mentor mentor;
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mentor_id")
+    private Mentor mentor;
 
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	LocalDate date;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    LocalDate date;
 
-	@JsonFormat(pattern = "HH:mm")  //datetimeformat은 ss까지 전부 다 받아야 오류안남
-	LocalTime startTime;
+    @JsonFormat(pattern = "HH:mm")  //datetimeformat은 ss까지 전부 다 받아야 오류안남
+    LocalTime startTime;
 
-	@JsonFormat(pattern = "HH:mm")
-	LocalTime endTime;
+    @JsonFormat(pattern = "HH:mm")
+    LocalTime endTime;
 
-	@Column
-	@Setter
-	@Builder.Default
-	private boolean isActive=true;
+    @Column
+    @Setter
+    @Builder.Default
+    private boolean isActive=true;
 
-	public static PossibleDate from(PossibleDateRequestDto dto) {
-		return PossibleDate.builder()
-			.date(dto.getDate())
-			.startTime(dto.getStartTime())
-			.endTime(dto.getEndTime())
-			.isActive(true)
-			.build();
-	}
+    public static PossibleDate from(PossibleDateRequestDto dto) {
+        return PossibleDate.builder()
+                .date(dto.getDate())
+                .startTime(dto.getStartTime())
+                .endTime(dto.getEndTime())
+                .isActive(true)
+                .build();
+    }
 }
--- src/main/java/com/soongsil/CoffeeChat/service/ApplicationService.java
+++ src/main/java/com/soongsil/CoffeeChat/service/ApplicationService.java
@@ -1,10 +1,14 @@
 package com.soongsil.CoffeeChat.service;
 
 import com.soongsil.CoffeeChat.entity.*;
+import com.soongsil.CoffeeChat.enums.ApplicationStatus;
 import com.soongsil.CoffeeChat.repository.PossibleDate.PossibleDateRepository;
 import jakarta.mail.MessagingException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Async;
@@ -25,6 +29,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
@@ -87,6 +92,49 @@ public class ApplicationService {
 	@Async("mailExecutor")
 	public void sendApplicationMatchedEmailAsync(String email, String mentorName, String menteeName, LocalDate date, LocalTime startTime, LocalTime endTime) throws MessagingException {
 		emailUtil.sendApplicationMatchedEmail(email, mentorName, menteeName, date, startTime, endTime);
+	}
+
+	//동시성 테스트용
+	private static final Logger logger = LoggerFactory.getLogger(ApplicationService.class);
+	private static final AtomicInteger transactionCounter = new AtomicInteger(0);  //트랜잭션마다 ID부여
+	@Transactional
+	public Application createApplicationIfPossible(Long possibleDateId, Mentor mentor, Mentee mentee) throws Exception {
+		int transactionId = transactionCounter.incrementAndGet();  //트랜잭션 ID 1씩 증가하며 부여
+		MDC.put("transactionId", String.valueOf(transactionId));  //로그에 트랜잭션ID 띄우기
+		MDC.put("threadId", String.valueOf(Thread.currentThread().getId())); //로그에 스레드ID 띄우기
+
+		try {
+			logger.info("aaa트랜잭션 시작");
+
+			PossibleDate possibleDate = em.find(PossibleDate.class, possibleDateId);
+
+			if (possibleDate != null && possibleDate.isActive()) {  //Active상태면, Application생성
+				possibleDate.setActive(false);  //중요! active상태를 false로 변경
+				em.merge(possibleDate);
+
+				Application application = Application.builder()
+						.mentor(mentor)
+						.mentee(mentee)
+						.date(possibleDate.getDate())
+						.startTime(possibleDate.getStartTime())
+						.endTime(possibleDate.getEndTime())
+						.accept(ApplicationStatus.UNMATCHED)
+						.build();
+				em.persist(application);
+
+				logger.info("aaaApplication 생성: {}", application);
+				return application;
+			} else {
+				logger.error("aaaAplication 생성 실패-Active하지 않음.");
+				throw new Exception("The PossibleDate is already booked or does not exist.");
+			}
+		} catch (Exception e) {
+			logger.error("aaaAplication 생성중 에러: ", e);
+			throw e;
+		} finally {
+			logger.info("aaa트랜잭션 종료");
+			MDC.clear();
+		}
 	}
 
 }
