6a60a2b6cd775450ee61381ec290b005555899e5
oxdjww <oxdjww@gmail.com>
2024-08-10 18:45:51 +0900

    refactor: mentorId 기반 서칭으로 리팩토링

--- src/main/java/com/soongsil/CoffeeChat/repository/PossibleDate/PossibleDateRepository.java
+++ src/main/java/com/soongsil/CoffeeChat/repository/PossibleDate/PossibleDateRepository.java
@@ -1,6 +1,7 @@
 package com.soongsil.CoffeeChat.repository.PossibleDate;
 
 import com.soongsil.CoffeeChat.dto.PossibleDateRequestDto;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.soongsil.CoffeeChat.entity.PossibleDate;
@@ -8,5 +9,5 @@ import com.soongsil.CoffeeChat.entity.PossibleDate;
 import java.util.List;
 
 public interface PossibleDateRepository extends JpaRepository<PossibleDate, Long>, PossibleDateRepositoryCustom {
-    List<PossibleDateRequestDto> getPossibleDatesByUsername(String username);
+	List<PossibleDateRequestDto> getPossibleDatesById(Long mentorId);
 }
