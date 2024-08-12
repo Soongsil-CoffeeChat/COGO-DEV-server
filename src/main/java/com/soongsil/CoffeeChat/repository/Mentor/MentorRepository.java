6918dfdb763dd7b9bfa2f48a54a4df8a6265d60f
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2024-08-12 21:38:24 +0900

    파트, 동아리의 타입 enum으로 교체후 쿼리수정

--- src/main/java/com/soongsil/CoffeeChat/repository/Mentor/MentorRepository.java
+++ src/main/java/com/soongsil/CoffeeChat/repository/Mentor/MentorRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.soongsil.CoffeeChat.dto.ResponseMentorListInfo;
 
 import com.soongsil.CoffeeChat.entity.User;
+import com.soongsil.CoffeeChat.enums.PartEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.soongsil.CoffeeChat.entity.Mentor;
@@ -12,7 +13,7 @@ import com.soongsil.CoffeeChat.entity.Mentor;
 public interface MentorRepository extends JpaRepository<Mentor, Long> , MentorRepositoryCustom{
 	//Mentor findByUsername(String username); 상속시 사용가능
 
-	List<Mentor> findAllByPart(String part);
-	List<ResponseMentorListInfo> getMentorListByPart(String part);  //일반 join
-	List<User> getMentorListByPartWithFetch(String part); //fetch join
+	List<Mentor> findAllByPart(PartEnum part);
+	List<ResponseMentorListInfo> getMentorListByPart(PartEnum part);  //일반 join
+	List<User> getMentorListByPartWithFetch(PartEnum part); //fetch join
 }
--- src/main/java/com/soongsil/CoffeeChat/repository/Mentor/MentorRepositoryImpl.java
+++ src/main/java/com/soongsil/CoffeeChat/repository/Mentor/MentorRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.soongsil.CoffeeChat.repository.Mentor;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.soongsil.CoffeeChat.dto.QResponseMentorListInfo;
 import com.soongsil.CoffeeChat.dto.ResponseMentorListInfo;
 
 import com.soongsil.CoffeeChat.entity.User;
+import com.soongsil.CoffeeChat.enums.ClubEnum;
+import com.soongsil.CoffeeChat.enums.PartEnum;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.soongsil.CoffeeChat.entity.QIntroduction.introduction;
+import static com.soongsil.CoffeeChat.entity.QMentor.mentor;
+import static com.soongsil.CoffeeChat.entity.QUser.user;
 
 
 public class MentorRepositoryImpl implements MentorRepositoryCustom{
@@ -18,8 +24,8 @@ public class MentorRepositoryImpl implements MentorRepositoryCustom{
     }
 
     @Override
-    public List<ResponseMentorListInfo> getMentorListByPart(int part) { //일반 join
-        /*
+    public List<ResponseMentorListInfo> getMentorListByPart(PartEnum part) { //일반 join
+
         return queryFactory
                 .select(new QResponseMentorListInfo(
                         user.picture,
@@ -31,20 +37,15 @@ public class MentorRepositoryImpl implements MentorRepositoryCustom{
                         introduction.title,
                         introduction.description
                         ))
-                .from(user)mentorcontro
+                .from(user)
                 .join(user.mentor, mentor)
                 .join(mentor.introduction, introduction)
                 .where(mentor.part.eq(part))
                 .fetch();
-
-         */
-        return new ArrayList<>();
-
     }
 
     @Override
-    public List<ResponseMentorListInfo> getMentorListByClub(int club) { //일반 join
-        /*
+    public List<ResponseMentorListInfo> getMentorListByClub(ClubEnum club) { //일반 join
         return queryFactory
                 .select(new QResponseMentorListInfo(
                         user.picture,
@@ -61,14 +62,10 @@ public class MentorRepositoryImpl implements MentorRepositoryCustom{
                 .join(mentor.introduction, introduction)
                 .where(mentor.club.eq(club))
                 .fetch();
-
-         */
-        return new ArrayList<>();
     }
 
     @Override
-    public List<ResponseMentorListInfo> getMentorListByPartAndClub(int part, int club) { //일반 join
-        /*
+    public List<ResponseMentorListInfo> getMentorListByPartAndClub(PartEnum part, ClubEnum club) { //일반 join
         return queryFactory
                 .select(new QResponseMentorListInfo(
                         user.picture,
@@ -85,22 +82,14 @@ public class MentorRepositoryImpl implements MentorRepositoryCustom{
                 .join(mentor.introduction, introduction)
                 .where(mentor.club.eq(club).and(mentor.part.eq(part)))
                 .fetch();
-
-         */
-        return new ArrayList<>();
     }
 
     @Override
-    public List<User> getMentorListByPartWithFetch(int part) {  //fetch join
-        /*
+    public List<User> getMentorListByPartWithFetch(PartEnum part) {  //fetch join
         return queryFactory
                 .selectFrom(user)
                 .join(user.mentor, mentor).fetchJoin()
                 .where(mentor.part.eq(part))
                 .fetch();
-
-         */
-        return new ArrayList<>();
-
     }
 }
