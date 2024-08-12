f4091fa1cf7391a3df6abf94a5e52c0670f8e7fc
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2024-08-12 18:44:58 +0900

    멘티 엔티티 수정, part타입 enum으로 변경

--- src/main/java/com/soongsil/CoffeeChat/dto/MenteeDto.java
+++ src/main/java/com/soongsil/CoffeeChat/dto/MenteeDto.java
@@ -1,8 +1,9 @@
 package com.soongsil.CoffeeChat.dto;
 
+import com.soongsil.CoffeeChat.enums.PartEnum;
 import lombok.Getter;
 
 @Getter
 public class MenteeDto {
-	private String part;
+	private PartEnum part;
 }
