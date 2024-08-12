d5fab95e5fff5b28d3dba37a56895a3205e989aa
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2024-08-12 18:44:58 +0900

    멘티 엔티티 수정, part타입 enum으로 변경

--- src/main/java/com/soongsil/CoffeeChat/entity/Mentee.java
+++ src/main/java/com/soongsil/CoffeeChat/entity/Mentee.java
@@ -5,13 +5,8 @@ import java.util.Set;
 
 import com.soongsil.CoffeeChat.dto.MenteeDto;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import com.soongsil.CoffeeChat.enums.PartEnum;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,44 +25,18 @@ public class Mentee {
 	private Long id;
 
 	@Column
-	private String picture;
-	//TODO: aws파지면 사진처리 해줘야됨
+	@Enumerated(EnumType.STRING)
+	private PartEnum part;
 
-	@Column(name = "phone_num")
-	private String phoneNum;
-
-	@Column
-	private String birth;
-
-	@Column
-	private int grade;
-
-	@Column
-	private String part;
-
-	@Column
-	private String memo;
-
-	@Column
-	private String nickname;
 
 	@OneToMany(mappedBy = "mentee", cascade = CascadeType.ALL, orphanRemoval = true)
 	@Builder.Default
 	private Set<Application> applications = new HashSet<>();
 
-	@Builder
-	public Mentee(String phoneNum, String birth, int grade, String part, String memo) {
-		this.phoneNum = phoneNum;
-		this.birth = birth;
-		this.grade = grade;
-		this.part = part;
-		this.memo = memo;
-	}
 
 	public static Mentee from(MenteeDto dto) {
 		return Mentee.builder()
 			.part(dto.getPart())
 			.build();
 	}
-
 }
