a6e71b1382219668580b5291672a44e39c989015
oxdjww <oxdjww@gmail.com>
2024-08-13 22:39:04 +0900

    fix: possibleDate 생성, 조회 기능 버그 수정 및 테스트 완료

--- src/main/java/com/soongsil/CoffeeChat/repository/PossibleDate/PossibleDateRepository.java
+++ src/main/java/com/soongsil/CoffeeChat/repository/PossibleDate/PossibleDateRepository.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.soongsil.CoffeeChat.dto.PossibleDateCreateGetDto;
 import com.soongsil.CoffeeChat.entity.PossibleDate;
 
 public interface PossibleDateRepository extends JpaRepository<PossibleDate, Long>, PossibleDateRepositoryCustom {
-	List<PossibleDateCreateGetDto> getPossibleDatesById(Long mentorId);
+	List<PossibleDate> getPossibleDatesByMentorId(Long mentorId);
 }
