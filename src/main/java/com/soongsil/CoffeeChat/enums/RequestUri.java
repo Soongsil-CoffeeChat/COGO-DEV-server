21c1f3e6bc706395d98990d12a541a2cd0fa79db
oxdjww <oxdjww@gmail.com>
2024-08-13 19:22:21 +0900

    refactor: security config URI 수정 및 Reissue uri 원복

--- src/main/java/com/soongsil/CoffeeChat/enums/RequestUri.java
+++ src/main/java/com/soongsil/CoffeeChat/enums/RequestUri.java
@@ -7,7 +7,7 @@ public class RequestUri {
 	public static final String MENTEE_URI = prefix + "/mentees";
 	public static final String MENTOR_URI = prefix + "/mentors";
 	public static final String POSSIBLEDATE_URI = prefix + "/possibleDates";
-	public static final String REFRESH_URI = "/auth/refresh";
+	// public static final String REFRESH_URI = "/reissue";
 	public static final String USER_URI = prefix + "/users";
 	public static final String EMAIL_URI = "/auth/email";
 }
