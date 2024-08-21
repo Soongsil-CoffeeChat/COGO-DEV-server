0842dccff8df37253f3cba74f0323a080e743634
oxdjww <oxdjww@gmail.com>
2024-08-21 22:29:10 +0900

    feat: 성공 핸들러 추가

--- src/main/java/com/soongsil/CoffeeChat/controller/handler/ApiResponseGenerator.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/handler/ApiResponseGenerator.java
@@ -1,5 +1,8 @@
 package com.soongsil.CoffeeChat.controller.handler;
 
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.soongsil.CoffeeChat.controller.exception.CustomException;
@@ -11,7 +14,7 @@ import lombok.NonNull;
 @Getter
 @JsonPropertyOrder({"statusCode", "message", "content"})
 @AllArgsConstructor
-public class ApiResponseGenerator {
+public class ApiResponseGenerator<T> {
 
 	@JsonProperty("statusCode")
 	@NonNull
@@ -21,11 +24,37 @@ public class ApiResponseGenerator {
 	@NonNull
 	private final String message;
 
-	public static ApiResponseGenerator onFailure(
+	@JsonProperty("content")
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private final T content;
+
+	public static <T> ApiResponseGenerator<T> onSuccess(
+		HttpStatus httpStatus,
+		String message,
+		T content
+	) {
+		return new ApiResponseGenerator<>(
+			String.valueOf(httpStatus.value()),
+			message,
+			content
+		);
+	}
+
+	public static <T> ApiResponseGenerator<T> onFailure(
 		CustomException customException) {
-		return new ApiResponseGenerator(
+		return new ApiResponseGenerator<>(
 			customException.getErrorCode().toString(),
-			customException.getErrorMessage()
+			customException.getErrorMessage(),
+			null
+		);
+	}
+
+	public static <T> ApiResponseGenerator<T> onFailure(
+		CustomException customException, T content) {
+		return new ApiResponseGenerator<>(
+			customException.getErrorCode().toString(),
+			customException.getErrorMessage(),
+			content
 		);
 	}
 }
