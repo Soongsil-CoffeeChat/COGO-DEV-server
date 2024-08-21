df058f0b42b1f592e1f52e3afdf1b5075c6a9d3f
oxdjww <oxdjww@gmail.com>
2024-08-22 01:18:25 +0900

    docs: swagger 정보 추가

--- src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.soongsil.CoffeeChat.controller.handler.ApiResponseGenerator;
 import com.soongsil.CoffeeChat.util.email.EmailUtil;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +25,8 @@ public class EmailController {
 	private final EmailUtil emailUtil;
 
 	@GetMapping()
+	@Operation(summary = "이메일 인증 코드 전송")
+	@ApiResponse(responseCode = "200", description = "이메일로 전송된 코드 반환")
 	public ResponseEntity<ApiResponseGenerator<Map<String, String>>> sendAuthenticationMail(
 		@RequestParam("email") String receiver) throws
 		MessagingException,
