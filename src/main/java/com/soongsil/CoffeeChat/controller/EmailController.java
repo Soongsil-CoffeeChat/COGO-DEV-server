e3e51d0ec969dfdd1e7c479e6fb239b42282b5e4
oxdjww <oxdjww@gmail.com>
2024-08-22 00:10:29 +0900

    refactor: 응답 양식 EmailController에 적용

--- src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
@@ -1,12 +1,16 @@
 package com.soongsil.CoffeeChat.controller;
 
 import static com.soongsil.CoffeeChat.enums.RequestUri.*;
+import static org.springframework.http.HttpStatus.*;
+
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soongsil.CoffeeChat.controller.handler.ApiResponseGenerator;
 import com.soongsil.CoffeeChat.util.email.EmailUtil;
 
 import jakarta.mail.MessagingException;
@@ -19,10 +23,16 @@ public class EmailController {
 	private final EmailUtil emailUtil;
 
 	@GetMapping()
-	public String sendAuthenticationMail(@RequestParam("email") String receiver) throws
+	public ApiResponseGenerator<Map<String, String>> sendAuthenticationMail(
+		@RequestParam("email") String receiver) throws
 		MessagingException,
 		InterruptedException {
-		return emailUtil.sendAuthenticationEmail(receiver);
-
+		return ApiResponseGenerator.onSuccess(
+			OK,
+			OK.getReasonPhrase(),
+			Map.of(
+				"code", emailUtil.sendAuthenticationEmail(receiver)
+			)
+		);
 	}
 }
