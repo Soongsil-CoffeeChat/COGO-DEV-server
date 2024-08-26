3ac630c03b14226280564ddcbe93e6797deb2cb0
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2024-08-27 06:02:28 +0900

    ref: 메일인증 비동기처리 개선 및 안정성 테스트코드 작성(#133)

--- src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
@@ -3,7 +3,6 @@ package com.soongsil.CoffeeChat.controller;
 import static com.soongsil.CoffeeChat.enums.RequestUri.*;
 
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,7 +27,7 @@ public class EmailController {
 	@GetMapping()
 	@Operation(summary = "이메일 인증 코드 전송")
 	@ApiResponse(responseCode = "200", description = "이메일로 전송된 코드 반환")
-	public ResponseEntity<ApiResponseGenerator<Map<String, CompletableFuture<String>>>> sendAuthenticationMail(
+	public ResponseEntity<ApiResponseGenerator<Map<String, String>>> sendAuthenticationMail(
 		@RequestParam("email") String receiver) throws
 		MessagingException,
 		InterruptedException {
