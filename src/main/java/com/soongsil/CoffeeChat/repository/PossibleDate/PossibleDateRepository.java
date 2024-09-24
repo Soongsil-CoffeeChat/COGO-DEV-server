a48b45050cc65e204508cb94b2332e0359b71b4c
oxdjww <oxdjww@gmail.com>
2024-09-24 19:42:12 +0900

    refactor: 가능시간 갱신

--- src/main/java/com/soongsil/CoffeeChat/repository/PossibleDate/PossibleDateRepository.java
+++ src/main/java/com/soongsil/CoffeeChat/repository/PossibleDate/PossibleDateRepository.java
@@ -3,9 +3,17 @@ package com.soongsil.CoffeeChat.repository.PossibleDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.soongsil.CoffeeChat.entity.Mentor;
 import com.soongsil.CoffeeChat.entity.PossibleDate;
 
 public interface PossibleDateRepository extends JpaRepository<PossibleDate, Long>, PossibleDateRepositoryCustom {
 	List<PossibleDate> getPossibleDatesByMentorId(Long mentorId);
+
+	@Modifying
+	@Query("DELETE FROM PossibleDate pd WHERE pd.mentor = :mentor")
+	void deleteAllByMentor(@Param("mentor") Mentor mentor);
 }
