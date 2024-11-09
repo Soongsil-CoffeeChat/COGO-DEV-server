92c65f35069ea1589079fabb35f107d96dcaf92e
oxdjww <oxdjww@gmail.com>
2024-11-09 15:33:56 +0900

    FIX: User 역할에 따른 Application 조회 로직 간소화

--- src/main/java/com/soongsil/CoffeeChat/entity/User.java
+++ src/main/java/com/soongsil/CoffeeChat/entity/User.java
@@ -26,35 +26,42 @@ import lombok.Setter;
 //@Inheritance(strategy = InheritanceType.JOINED)  //자식 : Mentor, Mentee
 //@DiscriminatorColumn // 하위 테이블의 구분 컬럼 생성(default = DTYPE)
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
 
-	@Column
-	private String username;
+    @Column
+    private String username;
 
-	@Column
-	private String name;
+    @Column
+    private String name;
 
-	@Column
-	private String email;
+    @Column
+    private String email;
 
-	@Column
-	private String role;
+    @Column
+    private String role;
 
-	@Column
-	private String phoneNum;  //전화번호
+    @Column
+    private String phoneNum;  //전화번호
 
-	@Column
-	private String picture;
+    @Column
+    private String picture;
 
-	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_mentor", referencedColumnName = "mentor_id")
-	private Mentor mentor;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_mentor", referencedColumnName = "mentor_id")
+    private Mentor mentor;
 
-	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_mentee", referencedColumnName = "mentee_id")
-	private Mentee mentee;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_mentee", referencedColumnName = "mentee_id")
+    private Mentee mentee;
 
+    public boolean isMentor() {
+        return this.mentor != null;
+    }
+
+    public boolean isMentee() {
+        return this.mentee != null;
+    }
 }
