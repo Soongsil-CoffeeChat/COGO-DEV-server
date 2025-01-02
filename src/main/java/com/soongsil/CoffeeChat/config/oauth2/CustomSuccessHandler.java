a67ae6382bc93dac0d6af2e20398fe4c6747c7bd
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2025-01-02 16:54:11 +0900

    config파일들 재정리

--- src/main/java/com/soongsil/CoffeeChat/config/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/soongsil/CoffeeChat/config/oauth2/CustomSuccessHandler.java
@@ -20,8 +20,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 
-//로그인이 성공했을 때 받은 데이터들을 바탕으로 JWT발급을 위한 핸들러
-/*
 @Component
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final JWTUtil jwtUtil;
@@ -48,124 +46,36 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                                         Authentication authentication) throws IOException, ServletException {
 
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
-
         String username = customUserDetails.getUsername();
+        String role = authentication.getAuthorities().iterator().next().getAuthority();
 
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
-        GrantedAuthority auth = iterator.next();
-        String role = auth.getAuthority();
-
-        //String accessToken = jwtUtil.createJwt("access", username, role, 600000L);  // 10분
-        String accessToken = jwtUtil.createJwt("access", username, role, 180000L);  // 10분
-        System.out.println("accessToken = " + accessToken);
-        String refreshToken = jwtUtil.createJwt("refresh", username, role, 86400000L); // 24시간
+        String accessToken = jwtUtil.createJwt("access", username, role, 600000L);
+        String refreshToken = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
         addRefreshEntity(username, refreshToken, 86400000L);
 
-        System.out.println("=====쿠키쿠키=====");
-        // Refresh 토큰 쿠키에 추가
+        // Add Cookies
         addSameSiteCookie(response, "refresh", refreshToken);
-        System.out.println("리프레쉬: " + refreshToken);
-        System.out.println("=====쿠키쿠키=====");
-
-        // loginStatus 쿠키 추가
-        if (role.equals("ROLE_USER")) {
-            addSameSiteCookie(response, "loginStatus", "signup");
-        } else if (role.equals("ROLE_MENTEE") || role.equals("ROLE_MENTOR")) {
-            addSameSiteCookie(response, "loginStatus", "main");
-        }
+        addSameSiteCookie(response, "loginStatus", role.equals("ROLE_USER") ? "signup" : "main");
 
         response.setStatus(HttpStatus.OK.value());
-        //response.sendRedirect("http://localhost:8080/swagger-ui/index.html"); //서버 로컬 테스트용
-        //response.sendRedirect("https://localhost:3000/callback");
         response.sendRedirect("https://coffeego-ssu.web.app/callback");
     }
 
     private void addSameSiteCookie(HttpServletResponse response, String name, String value) {
         ResponseCookie responseCookie = ResponseCookie.from(name, value)
-                .domain("coffeego-ssu.web.app")
                 .httpOnly(true)
                 .secure(true)
-                .path("/")
-                .maxAge(24 * 60 * 60)
                 .sameSite("None")
-                .build();
-
-        response.addHeader("Set-Cookie", responseCookie.toString());
-    }
-}
- */
-@Component
-public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    private final JWTUtil jwtUtil;
-    private final RefreshRepository refreshRepository;
-
-    public CustomSuccessHandler(JWTUtil jwtUtil, RefreshRepository refreshRepository) {
-        this.jwtUtil = jwtUtil;
-        this.refreshRepository = refreshRepository;
-    }
-
-    private void addRefreshEntity(String username, String refresh, Long expiredMs) {
-        Date date = new Date(System.currentTimeMillis() + expiredMs);
-
-        Refresh refreshEntity = new Refresh();
-        refreshEntity.setUsername(username);
-        refreshEntity.setRefresh(refresh);
-        refreshEntity.setExpiration(date.toString());
-
-        refreshRepository.save(refreshEntity);
-    }
-
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
-
-        CustomOAuth2User customUserDetails = (CustomOAuth2User)authentication.getPrincipal();
-
-        String username = customUserDetails.getUsername();
-
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
-        GrantedAuthority auth = iterator.next();
-        String role = auth.getAuthority();
-
-        //String accessToken = jwtUtil.createJwt("access", username, role, 600000L);  // 10분
-        String accessToken = jwtUtil.createJwt("access", username, role, 180000L);  // 10분
-        System.out.println("accessToken = " + accessToken);
-        String refreshToken = jwtUtil.createJwt("refresh", username, role, 86400000L); // 24시간
-
-        addRefreshEntity(username, refreshToken, 86400000L);
-
-        // Refresh 토큰 쿠키에 추가
-        addSameSiteCookie(response, "refresh", refreshToken);
-
-        // loginStatus 쿠키 추가
-        if (role.equals("ROLE_USER")) {
-            addSameSiteCookie(response, "loginStatus", "signup");
-        } else if (role.equals("ROLE_MENTEE") || role.equals("ROLE_MENTOR")) {
-            addSameSiteCookie(response, "loginStatus", "main");
-        }
-
-        response.setStatus(HttpStatus.OK.value());
-        //response.sendRedirect("http://localhost:8080/swagger-ui/index.html"); //서버 로컬 테스트용
-        //response.sendRedirect("https://localhost:3000/callback");
-        response.sendRedirect("https://coffeego-ssu.web.app/callback");
-    }
-
-    private void addSameSiteCookie(HttpServletResponse response, String name, String value) {
-        ResponseCookie responseCookie = ResponseCookie.from(name, value)
-                .httpOnly(true)
-                .secure(true)
-                .maxAge(24 * 60 * 60)
                 .domain(".coffeego-ssu.web.app")
                 .path("/")
-                .sameSite("None")
+                .maxAge(24 * 60 * 60)
                 .build();
 
         response.addHeader("Set-Cookie", responseCookie.toString());
     }
 }
+
 
 /*
 package com.soongsil.CoffeeChat.config.oauth2;
