e2e12c5a47ea70216f759f68b46bba515b680a29
Ares-10 <cholionking1@naver.com>
2025-03-12 01:03:34 +0900

    remove: sqs ses 비활성화

--- src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
+++ src/main/java/com/soongsil/CoffeeChat/controller/EmailController.java
@@ -11,33 +11,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soongsil.CoffeeChat.controller.handler.ApiResponseGenerator;
-import com.soongsil.CoffeeChat.util.email.EmailUtil;
+//import com.soongsil.CoffeeChat.util.email.EmailUtil;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping(EMAIL_URI)
-public class EmailController {
-	private final EmailUtil emailUtil;
-
-	@GetMapping()
-	@Operation(summary = "이메일 인증 코드 전송")
-	@ApiResponse(responseCode = "200", description = "이메일로 전송된 코드 반환")
-	public ResponseEntity<ApiResponseGenerator<Map<String, String>>> sendAuthenticationMail(
-		@RequestParam("email") String receiver) throws
-		MessagingException,
-		InterruptedException {
-		System.out.println("receiver = " + receiver);
-		return ResponseEntity.ok().body(
-			ApiResponseGenerator.onSuccessOK(
-				Map.of(
-					"code", emailUtil.sendAuthenticationEmail(receiver)
-				)
-			)
-		);
-	}
-}
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping(EMAIL_URI)
+//public class EmailController {
+//	private final EmailUtil emailUtil;
+//
+//	@GetMapping()
+//	@Operation(summary = "이메일 인증 코드 전송")
+//	@ApiResponse(responseCode = "200", description = "이메일로 전송된 코드 반환")
+//	public ResponseEntity<ApiResponseGenerator<Map<String, String>>> sendAuthenticationMail(
+//		@RequestParam("email") String receiver) throws
+//		MessagingException,
+//		InterruptedException {
+//		System.out.println("receiver = " + receiver);
+//		return ResponseEntity.ok().body(
+//			ApiResponseGenerator.onSuccessOK(
+//				Map.of(
+//					"code", emailUtil.sendAuthenticationEmail(receiver)
+//				)
+//			)
+//		);
+//	}
+//}
