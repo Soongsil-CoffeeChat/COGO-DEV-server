d9db71cfbda07ee53a16402e9ca44183b5747e96
Ares-10 <cholionking1@naver.com>
2025-03-30 14:41:15 +0900

    fix: 자기소개 안된 멘토 조회 x

--- src/main/java/com/soongsil/CoffeeChat/domain/repository/Mentor/MentorRepositoryCustom.java
+++ src/main/java/com/soongsil/CoffeeChat/domain/repository/Mentor/MentorRepositoryCustom.java
@@ -3,18 +3,11 @@ package com.soongsil.CoffeeChat.domain.repository.Mentor;
 import java.util.List;
 
 import com.soongsil.CoffeeChat.domain.dto.MentorResponse.*;
-import com.soongsil.CoffeeChat.domain.entity.User;
 import com.soongsil.CoffeeChat.domain.entity.enums.ClubEnum;
 import com.soongsil.CoffeeChat.domain.entity.enums.PartEnum;
 
 public interface MentorRepositoryCustom {
-    List<MentorListResponse> getMentorListByPart(PartEnum part); // 일반 join
-
-    List<MentorListResponse> getMentorListByClub(ClubEnum club);
-
     List<MentorListResponse> getMentorListByPartAndClub(PartEnum part, ClubEnum club);
-
-    List<User> getMentorListByPartWithFetch(PartEnum part); // fetch join
 
     MentorGetUpdateDetailResponse getMentorInfoByMentorId(Long mentorId);
 }
