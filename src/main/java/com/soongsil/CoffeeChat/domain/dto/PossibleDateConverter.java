499d839868e4c2bdbc4e3014fc64f1e5fea47629
Ares-10 <cholionking1@naver.com>
2025-04-02 16:53:56 +0900

    fix: PossibleDateConverter 누락 파일 추가

---
+++ src/main/java/com/soongsil/CoffeeChat/domain/dto/PossibleDateConverter.java
@@ -0,0 +1,30 @@
+package com.soongsil.CoffeeChat.domain.dto;
+
+import com.soongsil.CoffeeChat.domain.dto.PossibleDateRequest.*;
+import com.soongsil.CoffeeChat.domain.entity.Mentor;
+import com.soongsil.CoffeeChat.domain.entity.PossibleDate;
+
+public class PossibleDateConverter {
+    public static PossibleDate toEntity(PossibleDateCreateRequest request, Mentor mentor) {
+        PossibleDate possibleDate =
+                PossibleDate.builder()
+                        .date(request.getDate())
+                        .startTime(request.getStartTime())
+                        .endTime(request.getEndTime())
+                        .isActive(true)
+                        .build();
+        mentor.addPossibleDate(possibleDate);
+        return possibleDate;
+    }
+
+    public static PossibleDateResponse.PossibleDateCreateResponse toResponse(
+            PossibleDate possibleDate) {
+        return PossibleDateResponse.PossibleDateCreateResponse.builder()
+                .date(possibleDate.getDate())
+                .startTime(possibleDate.getStartTime())
+                .endTime(possibleDate.getEndTime())
+                .possibleDateId(possibleDate.getId())
+                .isActive(possibleDate.isActive())
+                .build();
+    }
+}
