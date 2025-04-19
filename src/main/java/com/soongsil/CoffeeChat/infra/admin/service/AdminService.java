abf7e8dc2f094e111f88153f39ed9f5b26a3d7e5
Ares-10 <cholionking1@naver.com>
2025-04-19 16:28:29 +0900

    feat: 어드민 컨트롤러에 유저 조회 api 추가

--- src/main/java/com/soongsil/CoffeeChat/infra/admin/service/AdminService.java
+++ src/main/java/com/soongsil/CoffeeChat/infra/admin/service/AdminService.java
@@ -1,9 +1,15 @@
 package com.soongsil.CoffeeChat.infra.admin.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.soongsil.CoffeeChat.domain.dto.UserConverter;
+import com.soongsil.CoffeeChat.domain.dto.UserResponse.UserInfoResponse;
+import com.soongsil.CoffeeChat.domain.entity.User;
 import com.soongsil.CoffeeChat.domain.entity.enums.Role;
+import com.soongsil.CoffeeChat.domain.repository.User.UserRepository;
 import com.soongsil.CoffeeChat.global.exception.GlobalErrorCode;
 import com.soongsil.CoffeeChat.global.exception.GlobalException;
 import com.soongsil.CoffeeChat.global.security.jwt.JwtUtil;
@@ -14,15 +20,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdminService {
     private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
     @Value("${admin.password}")
     private String adminPassword;
 
-    public String issueAdmin(String password) {
+    public String issueAdmin(Long userId, String password) {
         if (!password.equals(adminPassword)) {
             throw new GlobalException(GlobalErrorCode.ADMIN_INCORRECT_PASSWORD);
         }
-        String accessToken = jwtUtil.createAccessToken("어드민", Role.ROLE_ADMIN);
-        return accessToken;
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new GlobalException(GlobalErrorCode.USER_NOT_FOUND));
+        return jwtUtil.createAccessToken(user.getUsername(), Role.ROLE_ADMIN);
+    }
+
+    public List<UserInfoResponse> getUserInfoList(String password) {
+        if (!password.equals(adminPassword)) {
+            throw new GlobalException(GlobalErrorCode.ADMIN_INCORRECT_PASSWORD);
+        }
+        return userRepository.findAll().stream().map(UserConverter::toResponse).toList();
     }
 }
