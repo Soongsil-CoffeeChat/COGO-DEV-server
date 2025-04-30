5bb76cc552d5d1b6798a7c8b23ac6078a646f442
Ares-10 <cholionking1@naver.com>
2025-04-30 22:49:51 +0900

    feat: BaseEntity 클래스 추가

---
+++ src/main/java/com/soongsil/CoffeeChat/domain/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.soongsil.CoffeeChat.domain.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate private LocalDateTime createdAt;
+
+    @LastModifiedDate private LocalDateTime updatedDate;
+}
