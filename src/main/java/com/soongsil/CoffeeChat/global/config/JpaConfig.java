3267140a66366b164112418c1dd9d4c3b6682f3d
Ares-10 <cholionking1@naver.com>
2025-05-02 05:06:58 +0900

    feat: baseEntity 설정 파일 추가

---
+++ src/main/java/com/soongsil/CoffeeChat/global/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.soongsil.CoffeeChat.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {}
