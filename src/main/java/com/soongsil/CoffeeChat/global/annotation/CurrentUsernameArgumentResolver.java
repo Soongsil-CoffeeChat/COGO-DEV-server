07e5408424af371f200833cd9e702de25d370541
Ares-10 <cholionking1@naver.com>
2025-05-02 05:08:33 +0900

    feat: CurrentUsername 어노테이션 구현

---
+++ src/main/java/com/soongsil/CoffeeChat/global/annotation/CurrentUsernameArgumentResolver.java
@@ -0,0 +1,46 @@
+package com.soongsil.CoffeeChat.global.annotation;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.soongsil.CoffeeChat.global.exception.GlobalErrorCode;
+import com.soongsil.CoffeeChat.global.exception.GlobalException;
+import com.soongsil.CoffeeChat.global.security.oauth2.CustomOAuth2User;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUsernameArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(CurrentUsername.class) != null
+                && parameter.getParameterType().equals(String.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new GlobalException(GlobalErrorCode.UNAUTHORIZED);
+        }
+
+        // 인증 객체에서 사용자 이름 추출
+        if (authentication.getPrincipal() instanceof CustomOAuth2User) {
+            return ((CustomOAuth2User) authentication.getPrincipal()).getUsername();
+        }
+        return authentication.getName();
+    }
+}
