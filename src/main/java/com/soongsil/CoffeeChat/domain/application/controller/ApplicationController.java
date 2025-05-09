5d591e02fb56c2db927ee5fc88605263d1faabad
Ares-10 <cholionking1@naver.com>
2025-05-09 19:37:20 +0900

    remove: 불필요 import 문 삭제

--- src/main/java/com/soongsil/CoffeeChat/domain/application/controller/ApplicationController.java
+++ src/main/java/com/soongsil/CoffeeChat/domain/application/controller/ApplicationController.java
@@ -5,6 +5,7 @@ import static com.soongsil.CoffeeChat.global.uri.RequestUri.APPLICATION_URI;
 import java.net.URI;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
