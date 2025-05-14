f48fea1d478dd031ff0812fa9e6b8792aec9e19a
bricksky <skydh1214@naver.com>
2025-05-14 12:40:00 +0900

    Fix/245 FCMToken 외래키 저장 테이블 구현

--- src/main/java/com/soongsil/CoffeeChat/domain/fcmNotification/entity/FCMToken.java
+++ src/main/java/com/soongsil/CoffeeChat/domain/fcmNotification/entity/FCMToken.java
@@ -1,5 +1,31 @@
 package com.soongsil.CoffeeChat.domain.fcmNotification.entity;
 
+import com.soongsil.CoffeeChat.domain.mentor.entity.Mentor;
+import com.soongsil.CoffeeChat.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Member;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FCMToken {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "fcm_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private User user;
+
+
+    private String token;
+
+    public FCMToken(User user, String token) {
+        this.user = user;
+        this.token = token;
+    }
 }
