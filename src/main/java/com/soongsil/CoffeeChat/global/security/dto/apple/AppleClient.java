f49e8ab3589340c9c2cee7b16c07cd4b9644404e
bricksky <skydh1214@naver.com>
2025-05-27 02:49:09 +0900

    [REFACTOR] ApplePublicKeyProvider 구현: 애플로부터 공개 키 가져와 캐싱

--- src/main/java/com/soongsil/CoffeeChat/global/security/dto/apple/AppleClient.java
+++
@@ -1,9 +0,0 @@
-package com.soongsil.CoffeeChat.global.security.dto.apple;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
-@FeignClient(name = "appleApiClient", url = "https://appleid.apple.com/auth")
-public interface AppleClient {
-    @GetMapping("/keys")
-    ApplePublicKeyResponse applgetAppleAuthPublicKey();
-}
