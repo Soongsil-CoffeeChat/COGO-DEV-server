021cce4945d9222150a515f0154beaacdfea6a35
bricksky <skydh1214@naver.com>
2025-06-01 21:34:06 +0900

    [REFACTOR] Firebase 인증정보 환경변수화

--- src/main/java/com/soongsil/CoffeeChat/global/security/apple/AppleKeyConfig.java
+++ src/main/java/com/soongsil/CoffeeChat/global/security/apple/AppleKeyConfig.java
@@ -6,24 +6,33 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
 public class AppleKeyConfig {
-    @Value("${spring.security.oauth2.client.registration.apple.private-key-location}")
-    private Resource privateKeyResource;
+    private final AppleProperties appleProperties;
+    private final ResourceLoader resourceLoader;
+
+    public AppleKeyConfig(AppleProperties appleProperties, ResourceLoader resourceLoader) {
+        this.appleProperties = appleProperties;
+        this.resourceLoader = resourceLoader;
+    }
 
     @Bean
     public RSAPrivateKey applePrivateKey() throws Exception {
+        Resource privateKeyResource =
+                resourceLoader.getResource(appleProperties.getPrivateKeyLocation());
+
         try (InputStream is = privateKeyResource.getInputStream()) {
             String key =
                     new String(is.readAllBytes())
                             .replace("-----BEGIN PRIVATE KEY-----", "")
                             .replace("-----END PRIVATE KEY-----", "")
                             .replaceAll("\\s+", "");
+
             byte[] decoded = Base64.getDecoder().decode(key);
             PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
             KeyFactory kf = KeyFactory.getInstance("RSA");
