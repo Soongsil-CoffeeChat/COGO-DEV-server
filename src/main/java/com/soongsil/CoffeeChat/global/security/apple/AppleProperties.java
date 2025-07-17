70b82b4eb4633dea43f4ada33f3f28d9b10457a4
yjr <emilyryoo5307@gmail.com>
2025-07-17 10:34:51 +0900

    chore: 애플 로직 분리

--- src/main/java/com/soongsil/CoffeeChat/global/security/apple/AppleProperties.java
+++ src/main/java/com/soongsil/CoffeeChat/global/security/apple/AppleProperties.java
@@ -1,63 +1,63 @@
-package com.soongsil.CoffeeChat.global.security.apple;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
-@Component
-@ConfigurationProperties(prefix = "social-login.provider.apple")
-public class AppleProperties {
-    private String clientId;
-    private String teamId;
-    private String keyId;
-    private String privateKeyLocation;
-    private String privateKey;
-    private String redirectUri;
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getTeamId() {
-        return teamId;
-    }
-
-    public void setTeamId(String teamId) {
-        this.teamId = teamId;
-    }
-
-    public String getKeyId() {
-        return keyId;
-    }
-
-    public void setKeyId(String keyId) {
-        this.keyId = keyId;
-    }
-
-    public String getPrivateKeyLocation() {
-        return privateKeyLocation;
-    }
-
-    public String getPrivateKey() {
-        return privateKey;
-    }
-
-    public void setPrivateKey(String privateKey) {
-        this.privateKey = privateKey;
-    }
-
-    public void setPrivateKeyLocation(String privateKeyLocation) {
-        this.privateKeyLocation = privateKeyLocation;
-    }
-
-    public String getRedirectUri() {
-        return this.redirectUri;
-    }
-
-    public void setRedirectUri(String redirectUri) {
-        this.redirectUri = redirectUri;
-    }
-}
+//package com.soongsil.CoffeeChat.global.security.apple;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@ConfigurationProperties(prefix = "social-login.provider.apple")
+//public class AppleProperties {
+//    private String clientId;
+//    private String teamId;
+//    private String keyId;
+//    private String privateKeyLocation;
+//    private String privateKey;
+//    private String redirectUri;
+//
+//    public String getClientId() {
+//        return clientId;
+//    }
+//
+//    public void setClientId(String clientId) {
+//        this.clientId = clientId;
+//    }
+//
+//    public String getTeamId() {
+//        return teamId;
+//    }
+//
+//    public void setTeamId(String teamId) {
+//        this.teamId = teamId;
+//    }
+//
+//    public String getKeyId() {
+//        return keyId;
+//    }
+//
+//    public void setKeyId(String keyId) {
+//        this.keyId = keyId;
+//    }
+//
+//    public String getPrivateKeyLocation() {
+//        return privateKeyLocation;
+//    }
+//
+//    public String getPrivateKey() {
+//        return privateKey;
+//    }
+//
+//    public void setPrivateKey(String privateKey) {
+//        this.privateKey = privateKey;
+//    }
+//
+//    public void setPrivateKeyLocation(String privateKeyLocation) {
+//        this.privateKeyLocation = privateKeyLocation;
+//    }
+//
+//    public String getRedirectUri() {
+//        return this.redirectUri;
+//    }
+//
+//    public void setRedirectUri(String redirectUri) {
+//        this.redirectUri = redirectUri;
+//    }
+//}
