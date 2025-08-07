1e4bdeb2116225020c72c261c57aec6c9bd57921
yjr <emilyryoo5307@gmail.com>
2025-08-08 02:17:18 +0900

    feat: 신고 사유 ui에 맞게 변경 및 스웨거 설명 추가

--- src/main/java/com/soongsil/CoffeeChat/domain/report/dto/ReportDto.java
+++ src/main/java/com/soongsil/CoffeeChat/domain/report/dto/ReportDto.java
@@ -3,6 +3,8 @@ package com.soongsil.CoffeeChat.domain.report.dto;
 import com.soongsil.CoffeeChat.domain.report.entity.Report;
 import com.soongsil.CoffeeChat.domain.report.enums.ReportReason;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,8 +12,15 @@ import lombok.Getter;
 @Getter
 public class ReportDto {
     // private Long reporterId;
+    @NotNull
+    @Schema(description = "신고 대상 ID", example = "12")
     private Long reportedUserId;
+
+    @NotNull
+    @Schema(description = "신고 사유 코드", implementation = ReportReason.class)
     private ReportReason reason;
+
+    @Schema(description = "신고 세부 내용", example = "OOO님이 약속을 지키지 않았습니다.")
     private String additionalDetails;
 
     public static ReportDto from(Report report) {
--- src/main/java/com/soongsil/CoffeeChat/domain/report/enums/ReportReason.java
+++ src/main/java/com/soongsil/CoffeeChat/domain/report/enums/ReportReason.java
@@ -1,9 +1,15 @@
 package com.soongsil.CoffeeChat.domain.report.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "신고 사유 코드")
 public enum ReportReason {
-    ABUSIVE_LANGUAGE,
-    SPAM,
-    INAPPROPRIATE_CONTENT,
-    PRIVACY_VIOLATION,
+    @Schema(description = "목적이 다른 것 같아요")
+    PURPOSE_MISMATCH,
+    @Schema(description = "멘토링 중에 분쟁이 발생했어요")
+    CONFLICT,
+    @Schema(description = "멘토의 소속 및 인적사항이 거짓인 것 같아요")
+    FALSE_INFO,
+    @Schema(description = "기타 부적절한 행위가 있었어요")
     OTHER
 }
