0d5e277e35b3bc1be0f05f04b7432225bea2d38f
yjr <emilyryoo5307@gmail.com>
2025-08-30 13:05:22 +0900

    chore: dev controller profile 해제

--- src/main/java/com/soongsil/CoffeeChat/domain/chat/controller/DevChatEchoController.java
+++ src/main/java/com/soongsil/CoffeeChat/domain/chat/controller/DevChatEchoController.java
@@ -2,13 +2,11 @@ package com.soongsil.CoffeeChat.domain.chat.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-@Profile("dev")
 @Controller
 @RequiredArgsConstructor
 public class DevChatEchoController {
