e2223f18d1ff3caadb74190232280e544db6d77c
yjr <emilyryoo5307@gmail.com>
2025-09-02 17:10:04 +0900

    refactor: request uri 대문자로 통일

--- src/main/java/com/soongsil/CoffeeChat/global/uri/RequestUri.java
+++ src/main/java/com/soongsil/CoffeeChat/global/uri/RequestUri.java
@@ -1,14 +1,14 @@
 package com.soongsil.CoffeeChat.global.uri;
 
 public class RequestUri {
-    private static final String prefix = "/api/v2";
-    public static final String APPLICATION_URI = prefix + "/applications";
-    public static final String CLUB_URI = prefix + "/clubs";
-    public static final String MENTEE_URI = prefix + "/mentees";
-    public static final String MENTOR_URI = prefix + "/mentors";
-    public static final String POSSIBLEDATE_URI = prefix + "/possibleDates";
+    private static final String PREFIX = "/api/v2";
+    public static final String APPLICATION_URI = PREFIX + "/applications";
+    public static final String CLUB_URI = PREFIX + "/clubs";
+    public static final String MENTEE_URI = PREFIX + "/mentees";
+    public static final String MENTOR_URI = PREFIX + "/mentors";
+    public static final String POSSIBLEDATE_URI = PREFIX + "/possibleDates";
     // public static final String REFRESH_URI = "/reissue";
-    public static final String USER_URI = prefix + "/users";
+    public static final String USER_URI = PREFIX + "/users";
     public static final String EMAIL_URI = "/auth/email";
-    public static final String REPORT_URI = prefix + "/report";
+    public static final String REPORT_URI = PREFIX + "/report";
 }
