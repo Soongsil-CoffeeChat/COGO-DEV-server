6da96747d98e49314aee8696a0bc70fdb1c021bc
yjr <emilyryoo5307@gmail.com>
2025-10-28 10:46:23 +0900

    feat: reject reason enum 추가

---
+++ src/main/java/com/soongsil/CoffeeChat/domain/application/enums/ApplicationRejectReason.java
@@ -0,0 +1,10 @@
+package com.soongsil.CoffeeChat.domain.application.enums;
+
+public enum ApplicationRejectReason {
+    SCHEDULE_ISSUE,
+    QUESTION_DIFFICULTY,
+    ANOTHER_AREA,
+    NO_AFFORD,
+    HEALTH_PROBLEM,
+    OTHER
+}
