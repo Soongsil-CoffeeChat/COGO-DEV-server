6219203f2621815b05791f9c1b8d6dee5c073c77
yjr <emilyryoo5307@gmail.com>
2026-03-14 04:20:35 +0900

    fix: report 쿼리 오타 수정

--- src/main/java/com/soongsil/CoffeeChat/domain/report/repository/ReportRepository.java
+++ src/main/java/com/soongsil/CoffeeChat/domain/report/repository/ReportRepository.java
@@ -11,7 +11,7 @@ import com.soongsil.CoffeeChat.domain.report.entity.Report;
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
     // 신고한 유저 ID 목록 조회
-    @Query("SELECT r.reportedUerId FROM Report r WHERE r.reporterId = :reporterId")
+    @Query("SELECT r.reportedUserId FROM Report r WHERE r.reporterId = :reporterId")
     List<Long> findReportedUserIdsByReporterId(@Param("reporterId") Long reporterId);
 
     // 사용자 간 신고 내영 존재 확인
